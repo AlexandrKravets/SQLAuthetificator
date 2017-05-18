5c9673d53afebd3316ff0ab06f81ea835cf7ed32
AlexandrKravets <alexandr.kravetc@gmail.com>
2017-05-18 16:07:00 +0300

    7 commit (add Logout button)

--- app/src/main/java/com/example/kravets/sqlauthentificator/Display.java
+++ app/src/main/java/com/example/kravets/sqlauthentificator/Display.java
@@ -3,7 +3,10 @@ package com.example.kravets.sqlauthentificator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Kravets on 17.05.2017.
@@ -18,4 +21,16 @@ public class Display extends Activity {
         TextView welc = (TextView) findViewById(R.id.welcome);
         welc.setText(userName);
     }
+
+    public void onButtonClick(View v){
+
+
+        if (v.getId()==R.id.logoutBtn)
+        {
+            Intent i = new Intent(Display.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+
+
 }
