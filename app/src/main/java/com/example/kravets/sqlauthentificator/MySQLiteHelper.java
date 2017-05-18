85895c8e3c072a3aaf1463afbd7aabfdca7cbc59
AlexandrKravets <alexandr.kravetc@gmail.com>
2017-05-18 13:58:46 +0300

    4 commit

--- app/src/main/java/com/example/kravets/sqlauthentificator/MySQLiteHelper.java
+++ app/src/main/java/com/example/kravets/sqlauthentificator/MySQLiteHelper.java
@@ -12,13 +12,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "SoftGroup";
-    private static final String TABLE_FOR_USERS = "Users";
+    private static final String DATABASE_NAME = "softGroup.db";
+    private static final String TABLE_NAME = "users";
 
-    private static final String ID = "_id";
-    private static final String EMAIL = "email";
-    private static final String PASSWORD = "password";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_PASSWORD = "password";
 
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+            + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_EMAIL + " TEXT,"
+            + COLUMN_PASSWORD + " TEXT" + ")";
+
+    SQLiteDatabase db;
 
 
     public MySQLiteHelper(Context context) {
@@ -27,27 +32,26 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + TABLE_FOR_USERS + "("
-                + ID + " INTEGER PRIMARY KEY," + EMAIL + " TEXT,"
-                + PASSWORD + " TEXT" + ")";
-        db.execSQL(query);
-
+        db.execSQL(CREATE_TABLE);
+        this.db=db;
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+        this.onCreate(db);
     }
 
     public void addUser(User user) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(EMAIL, user.getEmail());
-        values.put(PASSWORD, user.getPassword());
+        values.put(COLUMN_EMAIL, user.getEmail());
+        values.put(COLUMN_PASSWORD, user.getPassword());
 
-        db.insert(TABLE_FOR_USERS, null, values);
-        db.execSQL("INSERT INTO Students VALUES (NULL, '" + user.getEmail() + "', '" + user.getPassword() + "')");
+        db.insert(TABLE_NAME, null, values);
+        db.execSQL("INSERT INTO users VALUES (NULL, '" + user.getEmail() + "', '" + user.getPassword() + "')");
         db.close(); // Closing database connection
     }
 
